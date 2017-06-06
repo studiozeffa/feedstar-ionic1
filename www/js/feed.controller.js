178ef54f8f616f3979eec28fc810bf98e1f2cf22
Tom Spencer <tom.spencer@studiozeffa.com>
2017-06-06 21:54:46 +0100

    Feed page

--- www/js/feed.controller.js
+++ www/js/feed.controller.js
@@ -2,6 +2,29 @@
 
 angular
   .module('feedstar')
-  .controller('FeedController', function($scope, FeedFactory) {
+  .controller('FeedController', function($scope, FeedFactory, $ionicLoading) {
     $scope.feedUrl = FeedFactory.retrieveFeedUrl();
+
+    $scope.refreshFeed = function() {
+      refreshFeedAndItems().finally(function() {
+        $scope.$broadcast('scroll.refreshComplete');
+      });
+    };
+
+    $ionicLoading.show({
+      template: '<ion-spinner icon="ripple"></ion-spinner><br>Loading'
+    });
+
+    refreshFeedAndItems().finally(function() {
+      $ionicLoading.hide();
+    });
+
+    function refreshFeedAndItems() {
+      return FeedFactory
+        .getItems($scope.feedUrl)
+        .then(function(response) {
+          $scope.feed = response.feed;
+          $scope.items = response.items;
+        });
+    }
   });
