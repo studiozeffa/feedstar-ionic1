ab1e7f7324aaec662fbc6803309cf664303fe463
Tom Spencer <tom.spencer@studiozeffa.com>
2017-06-07 08:46:48 +0100

    Settings page

--- www/js/feed.controller.js
+++ www/js/feed.controller.js
@@ -2,26 +2,51 @@
 
 angular
   .module('feedstar')
-  .controller('FeedController', function($scope, FeedFactory, $ionicLoading) {
-    $scope.feedUrl = FeedFactory.retrieveFeedUrl();
-
+  .controller('FeedController', function($scope, FeedFactory, $ionicLoading, $ionicModal) {
     $scope.refreshFeed = function() {
       refreshFeedAndItems().finally(function() {
         $scope.$broadcast('scroll.refreshComplete');
       });
     };
 
-    $ionicLoading.show({
-      template: '<ion-spinner icon="ripple"></ion-spinner><br>Loading'
-    });
+    $scope.openFeedSettings = function() {
+      $scope.modal.show();
+    };
 
-    refreshFeedAndItems().finally(function() {
-      $ionicLoading.hide();
-    });
+    $scope.changeFeed = function() {
+      FeedFactory.rememberFeedUrl($scope.feedSettings.url);
+      $scope.modal.hide();
+      loadFeed();
+    };
+
+    $scope.feedSettings = {
+      url: FeedFactory.retrieveFeedUrl()
+    };
+
+    createModal();
+    loadFeed();
+
+    function createModal() {
+      $ionicModal.fromTemplateUrl('html/settings.html', {
+        scope: $scope
+      }).then(function(modal) {
+        $scope.modal = modal;
+      });
+    }
+
+    function loadFeed() {
+      $ionicLoading.show({
+        template: '<ion-spinner icon="ripple"></ion-spinner><br>Loading'
+      });
+
+      refreshFeedAndItems().finally(function() {
+        $ionicLoading.hide();
+      });
+    }
 
     function refreshFeedAndItems() {
       return FeedFactory
-        .getItems($scope.feedUrl)
+        .getItems($scope.feedSettings.url)
         .then(function(response) {
           $scope.feed = response.feed;
           $scope.items = response.items;
